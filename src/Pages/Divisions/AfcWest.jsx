91f34678b52125e5830d6e40f23744bf93ed51b1
Arthur Araujo <arthurtute200@hotmail.com>
2024-09-30 23:31:03 -0300

    Adding responsiviness to the application

--- src/Pages/Divisions/AfcWest.jsx
+++ src/Pages/Divisions/AfcWest.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { QbApi } from "../../QbAPI";
 import { Link } from "react-router-dom";
-import css from "../../application.module.css"
+
 
 export default function AfcWest(){
     const [quarterbacks,setQuarterbacks] = useState([])
@@ -13,12 +13,12 @@ export default function AfcWest(){
 
     return(
         <section>
-            <ul className={css.qbsUl}>
+            <ul >
                 {
                 (quarterbacks.map((quarterbacks)=>(
-                    <li style={{listStyle: "none"}} className={css.qbsList} key={quarterbacks.name} onClick={()=>setToQb(quarterbacks.name)}>
+                    <li style={{listStyle: "none"}}  key={quarterbacks.name} onClick={()=>setToQb(quarterbacks.name)}>
                        <Link to = "/profile" ><img src={quarterbacks.pic} alt={quarterbacks.name} /></Link>
-                       <Link to = "/profile" style={{textDecoration: "none",textAlign:"center",fontWeight:"bold"}}> {quarterbacks.name}</Link>
+                       <Link to = "/profile" > <p>{quarterbacks.name}</p></Link>
                     </li>
             )
         )
--- src/RootContainer.jsx
+++ src/RootContainer.jsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom"
 import Home from "./Components/Home"
 
+
 export default function RootContainer(){
     
     return(
         <div>
-            <p>
+            <p  >
                 This site gives you some informations about the starting AFC Quarterbacks.
             </p>
-            <p>
+            <p >
                 Go through the AFC East, AFC West, AFC South and AFC North to know more about the guys who lead their teams.
             </p>
+           
             <>
                 <Home/>
             </>
